1de0e64813952fc71f7243d30f6f275f8a2333f5
robin <robinskilling@gmail.com>
2011-09-12 23:27:46 +0200

    fix redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource_or_scope)
+    admin_path
+  end
+
+  # Tell Devise to redirect after sign_out
+  def after_sign_out_path_for(resource_or_scope)
+    index_path
+  end
 end
