cc3c8716dd1d9140e04b3e96ec904fefb2322da4
robin <robinskilling@gmail.com>
2011-09-18 20:36:51 +0100

    http protect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :verify_user
+
 
   def after_sign_in_path_for(resource_or_scope)
     admin_path
@@ -9,4 +11,14 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     index_path
   end
+
+  def verify_user
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |user_name, password|
+        user_name == "wwd" && password == "blairdev"
+      end
+      warden.custom_failure! if performed?
+    end
+  end
+
 end
